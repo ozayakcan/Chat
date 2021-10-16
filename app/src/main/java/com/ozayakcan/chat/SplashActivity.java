a672667c017d087937eba58c1638a963fab5dd03
Özay Akcan <iletisim@ozayakcan.com.tr>
2021-10-16 13:17:47 +0300

    Giriş kontrolü eklendi. Giriş yapılmadıysa LoginActivity'ye yönlendiriyor.

--- app/src/main/java/com/ozayakcan/chat/SplashActivity.java
+++ app/src/main/java/com/ozayakcan/chat/SplashActivity.java
@@ -2,16 +2,31 @@ package com.ozayakcan.chat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class SplashActivity extends AppCompatActivity {
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-    
+import java.io.FileInputStream;
+
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user != null){
+            //Giriş yapıldı
+        }else{
+            //Giriş yapılmadı
+            overridePendingTransition(0,0);
+            startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+            finish();
+        }
     }
 }
