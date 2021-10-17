17beee0a594a05fbedc5754bc6652bbf31f61859
Özay Akcan <iletisim@ozayakcan.com.tr>
2021-10-17 15:31:47 +0300

    Giriş ekranına geçiş animasyonu eklendi. (Telefon - Onay Kodu)

--- app/src/main/java/com/ozayakcan/chat/LoginActivity.java
+++ app/src/main/java/com/ozayakcan/chat/LoginActivity.java
@@ -1,14 +1,77 @@
 package com.ozayakcan.chat;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static com.ozayakcan.chat.Utils.Animasyonlar.YatayGecisAnimasyonu;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    private LinearLayout girisLayout, onayLayout;
+    private EditText telefonNumarasi;
+    private Button girisBtn;
+    private boolean telefonOnayi = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        girisLayout = findViewById(R.id.girisLayout);
+        telefonNumarasi = findViewById(R.id.telefonNumarasi);
+        girisBtn = findViewById(R.id.girisBtn);
+        girisBtn.setOnClickListener(v -> Giris());
+        onayLayout = findViewById(R.id.onayLayout);
+    }
+
+    private void Giris(){
+        YatayGecisAnimasyonu(girisLayout, onayLayout);
+        telefonOnayi = true;
+    }
+    @Override
+    public void onBackPressed() {
+        if(telefonOnayi){
+            YatayGecisAnimasyonu(onayLayout, girisLayout);
+            telefonOnayi = false;
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK){
+            if (telefonOnayi){
+                YatayGecisAnimasyonu(onayLayout, girisLayout);
+                telefonOnayi = false;
+                return false;
+            } else{
+                return super.onKeyDown(keyCode, event);
+            }
+        }else{
+            return super.onKeyDown(keyCode, event);
+        }
+    }
+
+    @Override
+    public boolean onKeyLongPress(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_BACK){
+            if (telefonOnayi){
+                YatayGecisAnimasyonu(onayLayout, girisLayout);
+                telefonOnayi = false;
+                return false;
+            }else{
+                return super.onKeyLongPress(keyCode, event);
+            }
+        }else{
+            return super.onKeyLongPress(keyCode, event);
+        }
     }
 }
---
+++ app/src/main/java/com/ozayakcan/chat/Utils/Animasyonlar.java
@@ -0,0 +1,39 @@
+package com.ozayakcan.chat.Utils;
+
+import android.animation.AnimatorSet;
+import android.animation.ValueAnimator;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+
+public class Animasyonlar {
+
+    public static int varsayilanAnimasyonSuresi = 300;
+
+    public static void Boyut(View view, int suankiBoyut, int yeniBoyut, boolean dikey){
+        ValueAnimator valueAnimator = ValueAnimator.ofInt(suankiBoyut, yeniBoyut).setDuration(varsayilanAnimasyonSuresi);
+        valueAnimator.addUpdateListener(animation1 -> {
+            if (dikey){
+                view.getLayoutParams().height = (Integer) animation1.getAnimatedValue();
+                view.requestLayout();
+            }else{
+                view.getLayoutParams().width = (Integer) animation1.getAnimatedValue();
+                view.requestLayout();
+            }
+        });
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.setInterpolator(new AccelerateDecelerateInterpolator());
+        animatorSet.play(valueAnimator);
+        animatorSet.start();
+    }
+
+    public static void DikeyGecisAnimasyonu(View gizlenecekView, View gosterilecekView){
+        int layoutBoyutu = gizlenecekView.getHeight();
+        Boyut(gizlenecekView, gizlenecekView.getHeight(), 0, true);
+        Boyut(gosterilecekView, 0, layoutBoyutu, true);
+    }
+    public static void YatayGecisAnimasyonu(View gizlenecekView, View gosterilecekView){
+        int layoutBoyutu = gizlenecekView.getWidth();
+        Boyut(gizlenecekView, gizlenecekView.getWidth(), 0, false);
+        Boyut(gosterilecekView, 0, layoutBoyutu, false);
+    }
+}
