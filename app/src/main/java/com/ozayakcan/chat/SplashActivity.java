a837f341a48d908d1cf26ceffad31386407a4aec
Özay Akcan <iletisim@ozayakcan.com.tr>
2021-10-19 03:23:59 +0300

    Firebase bağlantı sorunu giderildi. (Safetynet kaynaklı)

--- app/src/main/java/com/ozayakcan/chat/SplashActivity.java
+++ app/src/main/java/com/ozayakcan/chat/SplashActivity.java
@@ -1,13 +1,27 @@
 package com.ozayakcan.chat;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.safetynet.SafetyNetClient;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.appcheck.FirebaseAppCheck;
+import com.google.firebase.appcheck.safetynet.SafetyNetAppCheckProviderFactory;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.ozayakcan.chat.login.LoginActivity;
+import com.ozayakcan.chat.login.RegisterActivity;
+
+import java.util.HashMap;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -15,14 +29,38 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
+        FirebaseApp.initializeApp(this);
+        FirebaseAppCheck firebaseAppCheck = FirebaseAppCheck.getInstance();
+        firebaseAppCheck.installAppCheckProviderFactory(
+                SafetyNetAppCheckProviderFactory.getInstance());
+        //FirebaseDatabase.getInstance().setPersistenceEnabled(true);
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if(user != null){
-            //Giriş yapıldı
+            //Giriş Yapıldı
+            DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users" );
+            reference.keepSynced(true);
+            reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if(snapshot.hasChild(user.getUid())){
+                        //Kullanıcı Kayıtlı
+                    }else{
+                        //Kullanıcı Kayıtlı Değil
+                        startActivity(new Intent(SplashActivity.this, RegisterActivity.class));
+                        overridePendingTransition(0,0);
+                        finish();
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
         }else{
             //Giriş yapılmadı
-            overridePendingTransition(0,0);
             startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+            overridePendingTransition(0,0);
             finish();
         }
     }
--- app/src/main/java/com/ozayakcan/chat/login/RegisterActivity.java
+++ app/src/main/java/com/ozayakcan/chat/login/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.ozayakcan.chat.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 
 import com.ozayakcan.chat.R;
 
@@ -12,5 +13,21 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK){
+            finish();
+            return false;
+        }
+        else{
+            return super.onKeyDown(keyCode, event);
+        }
     }
 }
