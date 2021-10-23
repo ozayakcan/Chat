c3ed4f136e1ed557960cd2b8ec8e4972ea0a722e
Özay Akcan <iletisim@ozayakcan.com.tr>
2021-10-23 21:48:01 +0300

    Artık girişte kişi ve dosya izni sormuyor.

--- chat-android/app/src/main/java/com/ozayakcan/chat/SSActivity.java
+++ chat-android/app/src/main/java/com/ozayakcan/chat/SSActivity.java
@@ -1,8 +1,6 @@
 package com.ozayakcan.chat;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
@@ -23,16 +21,12 @@ import com.google.firebase.database.ValueEventListener;
 import com.ozayakcan.chat.Login.LoginActivity;
 import com.ozayakcan.chat.Login.RegisterActivity;
 import com.ozayakcan.chat.Model.Kullanici;
-import com.ozayakcan.chat.Utils.Izinler;
 import com.ozayakcan.chat.Utils.SharedPreference;
 import com.ozayakcan.chat.Utils.Veritabani;
 
 public class SSActivity extends AppCompatActivity {
 
-    private final int KISILER_IZIN_KODU = 1453;
-    private final int DOSYA_IZIN_KODU = 1081;
     private SharedPreference sharedPreference;
-    private Izinler izinler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,40 +36,10 @@ public class SSActivity extends AppCompatActivity {
         FirebaseAppCheck firebaseAppCheck = FirebaseAppCheck.getInstance();
         firebaseAppCheck.installAppCheckProviderFactory(
                 SafetyNetAppCheckProviderFactory.getInstance());
-        izinler = new Izinler(SSActivity.this);
         sharedPreference = new SharedPreference(SSActivity.this);
-        if (izinler.KontrolEt(Manifest.permission.READ_CONTACTS)){
-            DosyaIzniniKontrolEt();
-        }else{
-            izinler.Sor(Manifest.permission.READ_CONTACTS, KISILER_IZIN_KODU);
-        }
-
+        KullaniciyiKontrolEt();
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode == KISILER_IZIN_KODU){
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                DosyaIzniniKontrolEt();
-            }else{
-                izinler.ZorunluIzinUyariKutusu(Manifest.permission.READ_CONTACTS, KISILER_IZIN_KODU);
-            }
-        }else if(requestCode == DOSYA_IZIN_KODU){
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                KullaniciyiKontrolEt();
-            }else{
-                izinler.ZorunluIzinUyariKutusu(Manifest.permission.READ_EXTERNAL_STORAGE, DOSYA_IZIN_KODU);
-            }
-        }
-    }
-    private void DosyaIzniniKontrolEt(){
-        if (izinler.KontrolEt(Manifest.permission.READ_EXTERNAL_STORAGE)){
-            KullaniciyiKontrolEt();
-        }else{
-            izinler.Sor(Manifest.permission.READ_EXTERNAL_STORAGE, DOSYA_IZIN_KODU);
-        }
-    }
     private void KullaniciyiKontrolEt() {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if(user != null){
