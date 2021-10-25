730521f312a4c56ab6e612c6aa108a587e39835e
Özay Akcan <iletisim@ozayakcan.com.tr>
2021-10-25 16:00:43 +0300

    Mesaj listesi tasarımı eklendi.

---
+++ chat-android/app/src/main/java/com/ozayakcan/chat/Adapter/MesajlarAdapter.java
@@ -0,0 +1,67 @@
+package com.ozayakcan.chat.Adapter;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ozayakcan.chat.Model.Kullanici;
+import com.ozayakcan.chat.Ozellik.Resimler;
+import com.ozayakcan.chat.Ozellik.Veritabani;
+import com.ozayakcan.chat.R;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class MesajlarAdapter extends RecyclerView.Adapter<MesajlarAdapter.ViewHolder> {
+
+    private Context mContext;
+    private Resimler resimler;
+    List<Kullanici> kullaniciList;
+
+    public MesajlarAdapter(Context mContext, List<Kullanici> kullaniciList){
+        this.mContext = mContext;
+        this.kullaniciList = kullaniciList;
+        resimler = new Resimler(mContext);
+    }
+
+    @NonNull
+    @Override
+    public MesajlarAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.kisi_listesi, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    @Override
+    public void onBindViewHolder(@NonNull MesajlarAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return kullaniciList.size();
+    }
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public LinearLayout kisi;
+        public CircleImageView profilResmi;
+        public TextView kisiBasHarfi, kisiAdi, kisiHakkinda;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            kisi = itemView.findViewById(R.id.kisi);
+            profilResmi = itemView.findViewById(R.id.profilResmi);
+            kisiBasHarfi = itemView.findViewById(R.id.kisiBasHarfi);
+            kisiAdi = itemView.findViewById(R.id.kisiAdi);
+            kisiHakkinda = itemView.findViewById(R.id.kisiHakkinda);
+        }
+    }
+}
