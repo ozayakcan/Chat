6c26de87b6cbede8c9d412e50b4bfde268ddca1e
Özay Akcan <iletisim@ozayakcan.com.tr>
2021-12-22 10:59:28 +0300

    Mesaj işlevleri menüsüne 'Daha Fazla' butonu eklendi.

--- app/src/main/java/com/ozayakcan/chat/MainActivity.java
+++ app/src/main/java/com/ozayakcan/chat/MainActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
